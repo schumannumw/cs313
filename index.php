ccfb3d82ef9da1aa74fb6b03611ac5edd94e3a2a
Ryan Schumann <schumannumw@gmail.com>
2014-01-15 23:54:14 +0000

    2nd upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+		<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" />
+    </head>
+    <body>
+		<div id="press">
+			<img src="letterpressryan.jpg" alt="Header">
+		</div>
+		<div id="main_nav">
+		<ul>
+			<li><a href="index.php" title="Go to home page">Home</a></li>
+			<li><a href="introduction.php" title="Introduction">Introduction</a></li>
+			<li><a href="Assignments.php" title="Assignments">Assignments</a></li>
+		</ul>
+        </div>
+		<div id="content">
+		<h2>Welcome to my home page</h2>
+		<p>More to come shortly...</p>
+		</div>
+       <footer id="page_footer">
+                    <div>
+                      <p>Last Updated: 1/16/2014</p> 
+					  <p>&copy; 2014, Ryan Schumann, All rights Reserved</p>
+                        </div>
+        </footer>
+	</body>
+</html>
